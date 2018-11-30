32ca5510d922b6b89e03b6fed3505c405c396af8
Andrei Popa <andreipopa@administrators-MacBook-Pro-3.local>
2018-11-30 20:06:01 +0200

    Moved scripts to top level folder

--- src/keychain/Command/MacCommand.java
+++ src/keychain/Command/MacCommand.java
@@ -16,14 +16,14 @@ public class MacCommand implements ICommand {
 
     public void exec(String command) {
         try{
-            final File file = new File(System.getProperty("user.dir") + "/src/keychain/Scripts/Connect.sh");
+            final File file = new File(System.getProperty("user.dir") + "/Scripts/Connect.sh");
             if(!file.canExecute() || !file.canRead() || !file.canWrite()) {
                 file.setReadable(true, false);
                 file.setExecutable(true, false);
                 file.setWritable(true, false);
             }
 
-            ProcessBuilder pb = new ProcessBuilder(System.getProperty("user.dir") + "/src/keychain/Scripts/Connect.sh");
+            ProcessBuilder pb = new ProcessBuilder(System.getProperty("user.dir") + "/Scripts/Connect.sh");
             Map<String, String> env = pb.environment();
             env.put("command", command);
             pb.start();
